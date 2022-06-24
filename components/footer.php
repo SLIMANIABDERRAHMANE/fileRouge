523b0663ada19a7fb9aaac479023d014df1bdc41
SLIMANIABDERRAHMANE <92023424+SLIMANIABDERRAHMANE@users.noreply.github.com>
2022-06-24 11:36:19 +0100

    last and first

---
+++ components/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>links of my website</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+      </div>
+
+      <div class="box">
+         <h3>Our Team</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact us</h3>
+         <a href="#"><i class="fas fa-phone"></i> +212 4825165435</a>
+         <a href="#"><i class="fas fa-envelope"></i> info.soccer-as@gmail.com </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+      </div>
+
+  
+
+</footer>
